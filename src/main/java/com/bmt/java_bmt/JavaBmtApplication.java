598173bac580c6014e0c573197ffab52a479f96c
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-08-21 12:57:13 +0700

    chore: create base for project

---
+++ src/main/java/com/bmt/java_bmt/JavaBmtApplication.java
@@ -0,0 +1,13 @@
+package com.bmt.java_bmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bmt/java_bmt/JavaBmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmt.java_bmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
