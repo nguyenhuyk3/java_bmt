867aca085c8aef4b550299c268dae982089a8b98
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-08-23 15:11:30 +0700

    chore: add Final for RedisKey

--- src/main/java/com/bmt/java_bmt/helpers/constants/RedisKey.java
+++ src/main/java/com/bmt/java_bmt/helpers/constants/RedisKey.java
@@ -1,13 +1,17 @@
 package com.bmt.java_bmt.helpers.constants;
 
-public class RedisKey {
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+@FieldDefaults(level = AccessLevel.PUBLIC, makeFinal = true)
+public final class RedisKey {
     // Registration
-    public static String IS_IN_REGISTRATION_PROCESS = "is_in_registration_process::";
-    public static String REGISTRATION_OTP = "registration_otp::";
-    public static String REGISTRATION_COMPLETION = "registration_completion::";
+    static String IS_IN_REGISTRATION_PROCESS = "is_in_registration_process::";
+    static String REGISTRATION_OTP = "registration_otp::";
+    static String REGISTRATION_COMPLETION = "registration_completion::";
 
     // Forgot password
-    public static String IS_IN_FORGOT_PASSWORD_PROCESS = "is_in_forgot_password_process::";
-    public static String FORGOT_PASSWORD_OTP = "forgot_password_otp::";
-    public static String FORGOT_PASSWORD_COMPLETION = "forgot_password_completion::";
+    static String IS_IN_FORGOT_PASSWORD_PROCESS = "is_in_forgot_password_process::";
+    static String FORGOT_PASSWORD_OTP = "forgot_password_otp::";
+    static String FORGOT_PASSWORD_COMPLETION = "forgot_password_completion::";
 }
