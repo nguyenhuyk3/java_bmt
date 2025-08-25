06a2ff8663c9fa05a63d41d9080538768ba4a6ca
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-08-25 15:32:16 +0700

    feature: add ElasticsearchConfiguration and implement indexFilm

---
+++ src/main/java/com/bmt/java_bmt/configurations/ElasticsearchConfiguration.java
@@ -0,0 +1,4 @@
+package com.bmt.java_bmt.configurations;
+
+public class ElasticsearchConfiguration {
+}
---
+++ src/main/java/com/bmt/java_bmt/dto/others/IFilmElasticsearchProjection.java
@@ -0,0 +1,4 @@
+package com.bmt.java_bmt.dto.others;
+
+public interface IFilmElasticsearchProjection {
+}
