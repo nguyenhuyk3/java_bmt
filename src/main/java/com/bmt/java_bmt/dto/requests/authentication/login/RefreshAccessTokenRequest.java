d9487a8dc63a4b964bfd038f4dd7b9e02c07a62d
nguyenhuyk3 <huykimcuong5@gmail.com>
2025-08-29 16:47:29 +0700

    fix: add some annotations for RefreshAccessTokenRequest

--- src/main/java/com/bmt/java_bmt/dto/requests/authentication/login/RefreshAccessTokenRequest.java
+++ src/main/java/com/bmt/java_bmt/dto/requests/authentication/login/RefreshAccessTokenRequest.java
@@ -2,13 +2,13 @@ package com.bmt.java_bmt.dto.requests.authentication.login;
 
 import jakarta.validation.constraints.NotBlank;
 
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 import lombok.experimental.FieldDefaults;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class RefreshAccessTokenRequest {
     @NotBlank(message = "Token không được để trống")
